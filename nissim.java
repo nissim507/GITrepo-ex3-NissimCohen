889a520906a71554c91a0a61bc0e3a794e30b29a
nissim cohen <nisim5070@gmail.com>
2022-11-15 17:54:44 +0200

    update first time

--- nissim.java
+++ nissim.java
@@ -1,7 +1,7 @@
 public class nissim{
 public static void main(String [] arg){
 for (int i=1;i<=10;i++){
-System.out.println("Nissim Cohen ..."+i);
+System.out.println("Nissim Cohen update first...");
 }
 }
 }
